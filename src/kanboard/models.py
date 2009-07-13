1f1a6849d48d54c90a3b59ca9998534960bac8f1
Chris Heisel <chris@heisel.org>
2009-07-13 14:01:55 -0400

    more fields

--- src/kanboard/models.py
+++ src/kanboard/models.py
@@ -2,14 +2,16 @@ from django.db import models
 
 class Card(models.Model):
     title = models.CharField(max_length=80)
-    board = models.ForeignKey("Board", related_name="cards")
     phase = models.ForeignKey("Phase", related_name="cards")
-    order = models.SmallIntegerField() #Order is within a step, steps are pegged to a board
-    
+    order = models.SmallIntegerField() #Order is within a phase, steps are pegged to a board
+    backlogged_at = models.DateTimeField()
+
     #Optional fields
+    started_at = models.DateTimeField(blank=True, null=True)
+    done_at = models.DateTimeField(blank=True, null=True)
     description = models.TextField(blank=True)
     size = models.CharField(max_length=80, blank=True)
-    color = models.CharField(max_length=7) #For #003399 style css colors
+    color = models.CharField(max_length=7, blank=True) #For #003399 style css colors
     ready = models.BooleanField()
     blocked = models.BooleanField()
 
@@ -26,6 +28,7 @@ class Phase(models.Model):
 
     #Optional fields
     description = models.TextField(blank=True)
+    limit = models.SmallIntegerField(blank=True, null=True)
         
 
 
