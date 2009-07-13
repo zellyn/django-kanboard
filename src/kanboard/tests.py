b88b92b7b5e4386e26c4f977c82178d4f94c31f5
Chris Heisel <chris@heisel.org>
2009-07-13 17:06:17 -0400

    Started on tests

--- src/kanboard/tests.py
+++ src/kanboard/tests.py
@@ -1,8 +1,35 @@
+import random
 from django.test import TestCase
 
-from kanboard.models import Card
+from kanboard.models import Board, Card, Phase
 
-class KanboardTests(TestCase):
-    def test_environment(self):
-        """Just make sure everything is set up correctly."""
-        self.assert_(True)
+class KanboardTestCase(TestCase):
+    def random_int(self):
+        return random.randint(1, 100)
+
+    def create_board(self, save=True, **kwargs):
+        if not kwargs:
+            index = self.random_int()
+            kwargs = {
+                'title': 'Test Board %s' % index,
+                'slug': 'test-board-%s' % index,
+            }
+                
+        b = self.create_object(Board, save, kwargs)
+        if save:
+            self.assert_(b.title)
+            self.assert_(b.slug)
+        return b
+
+    def create_object(self, klass, save=True, kwargs={}):
+        o = klass(**kwargs) 
+        if save:
+            o.save()
+            self.assert_(o.id, "Unable to save %s" % o)
+        return o
+
+class KanboardTests(KanboardTestCase):
+    def test_create(self):
+        b = self.create_board()
+        self.assert_(b.id)
+
