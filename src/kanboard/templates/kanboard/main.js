061a0db91c6909f67f0267e3a4c4265e249d30ce
Zellyn Hunter <zhunter@ajc.com>
2009-07-29 18:56:56 -0400

    Remove javascript into separate file, actually post useful data to "update"

---
+++ src/kanboard/templates/kanboard/main.js
@@ -0,0 +1,28 @@
+/*
+    // Clean version
+    $(function() {
+      $('.sortable').sortable({connectWith: '.sortable'}).disableSelection();
+    });
+//*/
+//*
+    $(function() {
+      var debug = function(mesg) {
+        $('#messages').append('<li>' + mesg + '</li>');
+      };
+      $('.sortable').sortable({
+        connectWith: '.sortable',
+        stop: function(e, ui) {
+          var sender = ui.sender ? ui.sender[0].id : "(none)";
+          var item = ui.item[0].id;
+          var target = e.target.id;
+          debug('ui.sender=' + sender + ', item=' + item + ', target=' + target);
+          var data = {};
+          $('ul.phase').each(function() {
+            data[this.id] = $.map($(this).find('li.card'), function(y) { return y.id})
+          })
+          $.post('update/', data);
+        }
+      }).disableSelection();
+    });
+//*/
+
