08bb24ad80db72457c87533288b97942cc178dd6
Chris Beaven <chris@chris-desktop.(none)>
2009-11-22 13:42:48 +1300

    Remove static file serving (using django-staticfiles instead is recommended)

--- src/kanboard/urls.py
+++ src/kanboard/urls.py
@@ -1,14 +1,7 @@
-import os
 from django.conf.urls.defaults import patterns, url
-import kanboard
 
 urlpatterns = patterns('kanboard.views',
    url(r'^board/(?P<board_slug>[\w-]+)/$', 'board'),
    url(r'^board/(?P<board_slug>[\w-]+)/update/$', 'update'),
 )
 
-# Serve static content
-static_root = os.path.join(os.path.dirname(kanboard.__file__), 'static')
-urlpatterns += patterns('',
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': static_root})
-)
